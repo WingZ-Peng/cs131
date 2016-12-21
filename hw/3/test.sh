7e09acfa3cfd12d573bc911cd5c7d4ad8d67db5e
omniaura <omniaura5@gmail.com>
2016-12-21 15:51:52 -0700

    added test script

---
+++ hw/3/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# `Unsynchronized` and `GetNSet` omitted because they can deadlock
+for class in Synchronized #Null BetterSafe BetterSorry
+do
+  echo "testing ${class}..."
+  for nthreads in 1 2 4 8 16 32
+  do
+    echo "  with ${nthreads} thread(s)..."
+    for i in {1..100}
+    do 
+      java UnsafeMemory "$class" "$nthreads" 100000 6 5 6 3 0 3 | \
+      grep -oE ' \d+' >> \
+      "results.${class}.txt"
+    done
+  done
+done
