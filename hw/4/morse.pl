26376084ed0f6805d7a056de9b2d4036ff18a3ac
omniaura <omniaura5@gmail.com>
2017-01-12 12:17:10 -0800

    working on updated hw4

---
+++ hw/4/morse.pl
@@ -0,0 +1,85 @@
+% separator:       0 or 00
+% letter boundary: 000 or 00 or 0000
+% word boundary:   0000000 or 00000+
+% dih:             1 or 11
+% dah:             111 or 11+
+
+% unambiguous representations
+representation(separator, [0]). % separates adjancent dih and dah
+representation(., [1]). % dih
+representation(-, [1,1|Rest]). % dah
+representation(^, [0,0,0]). % letter boundary
+representation(#, [0,0,0,0,0|Rest]). % word boundary
+
+% ambiguous representations
+representation(separator, [0,0]). % separates adjancent dih and dah
+representation(., [1,1]). % dih
+representation(^, [0,0]). % letter boundary
+representation(^, [0,0,0,0]). % letter boundary
+
+% morse knowledge base from spec
+morse(a, [.,-]).           % A
+morse(b, [-,.,.,.]).	   % B
+morse(c, [-,.,-,.]).	   % C
+morse(d, [-,.,.]).	   % D
+morse(e, [.]).		   % E
+morse('e''', [.,.,-,.,.]). % É (accented E)
+morse(f, [.,.,-,.]).	   % F
+morse(g, [-,-,.]).	   % G
+morse(h, [.,.,.,.]).	   % H
+morse(i, [.,.]).	   % I
+morse(j, [.,-,-,-]).	   % J
+morse(k, [-,.,-]).	   % K or invitation to transmit
+morse(l, [.,-,.,.]).	   % L
+morse(m, [-,-]).	   % M
+morse(n, [-,.]).	   % N
+morse(o, [-,-,-]).	   % O
+morse(p, [.,-,-,.]).	   % P
+morse(q, [-,-,.,-]).	   % Q
+morse(r, [.,-,.]).	   % R
+morse(s, [.,.,.]).	   % S
+morse(t, [-]).	 	   % T
+morse(u, [.,.,-]).	   % U
+morse(v, [.,.,.,-]).	   % V
+morse(w, [.,-,-]).	   % W
+morse(x, [-,.,.,-]).	   % X or multiplication sign
+morse(y, [-,.,-,-]).	   % Y
+morse(z, [-,-,.,.]).	   % Z
+morse(0, [-,-,-,-,-]).	   % 0
+morse(1, [.,-,-,-,-]).	   % 1
+morse(2, [.,.,-,-,-]).	   % 2
+morse(3, [.,.,.,-,-]).	   % 3
+morse(4, [.,.,.,.,-]).	   % 4
+morse(5, [.,.,.,.,.]).	   % 5
+morse(6, [-,.,.,.,.]).	   % 6
+morse(7, [-,-,.,.,.]).	   % 7
+morse(8, [-,-,-,.,.]).	   % 8
+morse(9, [-,-,-,-,.]).	   % 9
+morse(., [.,-,.,-,.,-]).   % . (period)
+morse(',', [-,-,.,.,-,-]). % , (comma)
+morse(:, [-,-,-,.,.,.]).   % : (colon or division sign)
+morse(?, [.,.,-,-,.,.]).   % ? (question mark)
+morse('''',[.,-,-,-,-,.]). % ' (apostrophe)
+morse(-, [-,.,.,.,.,-]).   % - (hyphen or dash or subtraction sign)
+morse(/, [-,.,.,-,.]).     % / (fraction bar or division sign)
+morse('(', [-,.,-,-,.]).   % ( (left-hand bracket or parenthesis)
+morse(')', [-,.,-,-,.,-]). % ) (right-hand bracket or parenthesis)
+morse('"', [.,-,.,.,-,.]). % " (inverted commas or quotation marks)
+morse(=, [-,.,.,.,-]).     % = (double hyphen)
+morse(+, [.,-,.,-,.]).     % + (cross or addition sign)
+morse(@, [.,-,-,.,-,.]).   % @ (commercial at)
+
+% Error.
+morse(error, [.,.,.,.,.,.,.,.]). % error - see below
+
+% Prosigns.
+morse(as, [.,-,.,.,.]).          % AS (wait A Second)
+morse(ct, [-,.,-,.,-]).          % CT (starting signal, Copy This)
+morse(sk, [.,.,.,-,.,-]).        % SK (end of work, Silent Key)
+morse(sn, [.,.,.,-,.]).          % SN (understood, Sho' 'Nuff)
+
+
+signal_morse(I, O).
+
+
+signal_message(I, O).
